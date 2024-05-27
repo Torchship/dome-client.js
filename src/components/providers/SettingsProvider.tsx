886db8a82f2b82a12a1ec51109d861165fff6bc3
biscuitWizard <1904063+biscuitWizard@users.noreply.github.com>
2024-05-27 00:18:58 -0700

    Settings can now be saved

--- src/components/providers/SettingsProvider.tsx
+++ src/components/providers/SettingsProvider.tsx
@@ -1,13 +1,19 @@
-import React, { createContext, useContext, useState, ReactNode, useRef } from 'react';
+import React, { createContext, useContext, useState, ReactNode, useRef, useEffect } from 'react';
 import Settings, { DEFAULT_SETTINGS } from '../../models/Settings';
 
 interface SettingsContextProps {
   isSettingsOpen: boolean;
   setSettingsOpen: React.Dispatch<React.SetStateAction<boolean>> | null;
   settings: Settings;
+  saveSettings: () => void;
 }
 
-const SettingsContext = createContext<SettingsContextProps>({ isSettingsOpen: false, setSettingsOpen: null, settings: DEFAULT_SETTINGS });
+const SettingsContext = createContext<SettingsContextProps>({ 
+  isSettingsOpen: false, 
+  setSettingsOpen: null, 
+  settings: DEFAULT_SETTINGS, 
+  saveSettings: () => {}
+});
 
 export const useSettings = () => useContext(SettingsContext);
 
@@ -19,8 +25,22 @@ export const SettingsProvider: React.FC<SettingsProviderProps> = ({ children }) 
   const [isSettingsOpen, setSettingsOpen] = useState<boolean>(false);
   const settings = useRef<Settings>(DEFAULT_SETTINGS);
 
+  const saveSettings = () => {
+    localStorage.setItem("settings", JSON.stringify(settings.current));
+  };
+
+  const loadSettings = () => {
+    const rawSettings = localStorage.getItem("settings");
+    if (!rawSettings) return;
+    settings.current = JSON.parse(rawSettings);
+  }
+
+  useEffect(() => {
+    loadSettings();
+  }, []);
+
   return (
-    <SettingsContext.Provider value={{ isSettingsOpen, setSettingsOpen, settings: settings.current }}>
+    <SettingsContext.Provider value={{ isSettingsOpen, setSettingsOpen, settings: settings.current, saveSettings }}>
       {children}
     </SettingsContext.Provider>
   );
