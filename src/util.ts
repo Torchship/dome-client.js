4b70a8a65c6938df7f92fc8e05b204d8f2a3c3b5
biscuitWizard <1904063+biscuitWizard@users.noreply.github.com>
2024-05-27 02:38:51 -0700

    Added deep clone

---
+++ src/util.ts
@@ -0,0 +1,21 @@
+export function deepClone<T>(obj: T): T {
+  if (obj === null || typeof obj !== 'object') {
+    return obj;
+  }
+
+  if (Array.isArray(obj)) {
+    const arrCopy: any[] = [];
+    for (const item of obj) {
+      arrCopy.push(deepClone(item));
+    }
+    return arrCopy as unknown as T;
+  }
+
+  const objCopy: { [key: string]: any } = {};
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      objCopy[key] = deepClone((obj as { [key: string]: any })[key]);
+    }
+  }
+  return objCopy as T;
+}
