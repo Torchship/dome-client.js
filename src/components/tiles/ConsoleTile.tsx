a14c0afddca1e97220181e7dc8664099a429ae75
biscuitWizard <1904063+biscuitWizard@users.noreply.github.com>
2024-06-01 01:21:13 -0700

    Readded console controls

--- src/components/tiles/ConsoleTile.tsx
+++ src/components/tiles/ConsoleTile.tsx
@@ -4,12 +4,36 @@ import TileComponentType, { TileToolbarType } from '../TileComponent';
 import './ConsoleTile.css';
 import Button from '../Button';
 import VirtualTerminal from '../VirtualTerminal';
+import { useSettings } from '../providers/SettingsProvider';
+import { deepClone } from '../../util';
 
 export const ConsoleTileToolbar: TileToolbarType = () => {
+  const { settings, setSettings } = useSettings();
+
+  const toggleEcho = () => {
+    const newSettings = deepClone(settings);
+    newSettings.inputEcho = !settings.inputEcho;
+    setSettings(newSettings);
+  };
+
+  const toggleAutoScroll = () => {
+    const newSettings = deepClone(settings);
+    newSettings.autoscroll = !settings.autoscroll;
+    setSettings(newSettings);
+  }
+  
   return (
     <>
-      <Button label='Echo' className='toolbar-button'/>
-      <Button label='Pause Scroll' className='toolbar-button'/>
+      <Button 
+        label='Echo' 
+        onClick={toggleEcho} 
+        className='toolbar-button'
+        color={!settings.inputEcho ? 'info' : 'warning'} />
+      <Button 
+        label={settings.autoscroll ? "Pause Scroll" : "Resume Scroll"}
+        color={settings.autoscroll ? 'info' : 'warning'}
+        onClick={toggleAutoScroll} 
+        className='toolbar-button' />
     </>
   );
 };
