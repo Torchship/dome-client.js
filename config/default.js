ed6b028f7d6f74a70236213195a2fde5ffa5e798
biscuitWizard <1904063+biscuitWizard@users.noreply.github.com>
2025-04-15 18:30:19 -0700

    Update default.js

--- config/default.js
+++ config/default.js
@@ -11,7 +11,7 @@ module.exports = {
     // specific ip is optional (if your server has more than one)
     //    'ip'           : '208.52.189.89',
 
-    socketUrl: "http://play.torchship.org:5000",
+    socketUrl: "http://torchship.org:5000",
     socketUrlSSL: "",
     poweredBy: "dome-client.js",
     session: {
